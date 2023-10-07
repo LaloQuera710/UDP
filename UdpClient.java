2ce8194d62266285377d04000963dfbacc1810b3
STR <92766928+LaloQuera710@users.noreply.github.com>
2023-10-07 04:13:19 -0600

    Update UdpClient.java

--- UdpClient.java
+++ UdpClient.java
@@ -6,17 +6,17 @@ import java.net.*;
 public class UdpClient {
     public static void main(String args[]) throws Exception {
 // get server address and open socket
-        InetAddress 172.31.196.3 = 192.168.100.87:5555 .getByName(args[0]);
+        InetAddress 172.31.196.3 = 192.168.100.87 .getByName(args[1]);
         DatagramSocket sock = new DatagramSocket();
 // build packet addressed to server, then send it
         byte[] outBuf = args[1].getBytes("US-ASCII");
-        DatagramPacket outPkt = new
-                DatagramPacket(outBuf, outBuf.length, serverAdr, 9876);
+        DatagramPacket outPkt = 172.31.196.3
+                DatagramPacket(outBuf, outBuf.length, serverAdr, 5555);
         sock.send(outPkt);
         System.out.println("Sent...");
 // create buffer and packet for reply, then receive it
-        byte[] inBuf = new byte[1000];
-        DatagramPacket inPkt = new DatagramPacket(inBuf, inBuf.length);
+        byte[] inBuf = 172.31.196.3 byte[1000];
+        DatagramPacket inPkt = 192.168.100.87 DatagramPacket(inBuf, inBuf.length);
         sock.receive(inPkt);
         System.out.println("Recevied...");
 // print buffer contents and close socket
